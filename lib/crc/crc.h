517ea7972dc861174c73d23304b003e68fdf141a
Adel Mamin <adel.mamin@exafore.com>
2024-09-01 19:22:07 +0300

    Add crc

---
+++ lib/crc/crc.h
@@ -0,0 +1,134 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2018 Adel Mamin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * @file
+ * CRC utilities.
+ */
+
+#ifndef CRC_H_INCLUDED
+#define CRC_H_INCLUDED
+
+/**
+ * Compute CRC16.
+ *
+ * The parameter set:
+ * Name   : "CRC-16/CITT-FALSE"
+ * Width  : 16
+ * Poly   : 0x1021
+ * Init   : 0xFFFF
+ * RefIn  : False
+ * RefOut : False
+ * XorOut : 0x0000
+ *
+ * See
+ * http://reveng.sourceforge.net/crc-catalogue/16.htm#crc.cat.crc-16-ccitt-false
+ * for details.
+ *
+ * @param data  data for which the checksum is to be calculated
+ *              NULL is a valid value if size is zero.
+ * @param size  data size [bytes]
+ * @param crc   initial CRC value
+ *              Must be 0xFFFF at start or the result of previous crc16() call,
+ *              if the data is provided by chunks, in which case crc
+ *              is the CRC value of the previous data chunk.
+ * @return the calculated CRC value.
+ */
+unsigned int crc16(const unsigned char *data, int size, unsigned int crc);
+
+/**
+ * Compute CRC24.
+ *
+ * The parameter set:
+ * Name   : "CRC-24/LTE-A"
+ * Width  : 24
+ * Poly   : 0x864cfb
+ * Init   : 0x000000
+ * RefIn  : False
+ * RefOut : False
+ * XorOut : 0x000000
+ * Check  : 0xcde703
+ *
+ * See
+ * http://reveng.sourceforge.net/crc-catalogue/17plus.htm#crc.cat-bits.24
+ * for details.
+ *
+ * @param data  data for which the checksum is to be calculated
+ *              NULL is a valid value if size is zero.
+ * @param size  data array size [bytes]
+ * @param crc   initial CRC value
+ *              Must be 0x000000 at start or the result of previous crc24()
+ *              call, if the data is provided by chunks, in which case \a crc
+ *              is the CRC value of the previous data chunk.
+ * @return the calculated CRC value.
+ */
+unsigned long crc24(const unsigned char *data, int size, unsigned long crc);
+
+/**
+ * Compute CRC32.
+ *
+ * The parameter set:
+ * Name   : "CRC-32/MPEG-2"
+ * Width  : 32
+ * Poly   : 0x04C11DB7
+ * Init   : 0xFFFFFFFF
+ * RefIn  : False
+ * RefOut : False
+ * XorOut : 0x00000000
+ * Check  : 0x0376e6e7
+ *
+ * See
+ * http://www.ross.net/crc/download/crc_v3.txt
+ * http://reveng.sourceforge.net/crc-catalogue/17plus.htm (CRC-32/MPEG-2)
+ * for details.
+ *
+ * @param data  data for which the checksum is to be calculated
+ *              NULL is a valid value if size is zero
+ * @param size  data array size [bytes]
+ * @param crc   initial CRC value
+ *              Must be 0xFFFFFFFF at start or the result of previous crc32()
+ *              call, if the data is provided by chunks, in which case crc
+ *              is the CRC value of the previous data chunk.
+ * @return the calculated CRC value
+ */
+unsigned long crc32(const unsigned char *data, int size, unsigned long crc);
+
+/**
+ * The 8 bit Fletcher algorithm implementation.
+ * Reference: RFC-1145 (used in the TCP standard)
+ * @param data  data buffer
+ * @param size  data buffer size [bytes]
+ * @param ch_a  first 8 bit checksum
+ *              Clear to zero before the first call.
+ * @param ch_b  second 8 bit checksum
+ *              Clear to zero before the first call.
+ */
+void crc_fletcher8(
+    const unsigned char *data,
+    int size,
+    unsigned char *ck_a,
+    unsigned char *ck_b
+);
+
+#endif /* CRC_H_INCLUDED */
---
+++ lib/crc/test.c
@@ -0,0 +1,119 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2019 Adel Mamin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "common/compiler.h"
+#include "common/macros.h"
+#include "crc/crc.h"
+
+static void test_crc16(void) {
+    {
+        unsigned char data[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        static const unsigned int crc_init = 0xFFFF;
+        unsigned int crc = crc16(data, COUNT_OF(data), crc_init);
+        ASSERT(0x29b1 == crc);
+
+        crc = crc_init;
+        for (int i = 0; i < COUNT_OF(data); i++) {
+            crc = crc16(&data[i], 1, crc);
+        }
+        ASSERT(0x29b1 == crc);
+    }
+
+    {
+        unsigned char data[] = {
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '0'
+        };
+        static const unsigned int crc_init = 0xFFFF;
+        unsigned int crc = crc16(data, COUNT_OF(data), crc_init);
+        data[9] = (unsigned char)(crc >> 8u);
+        data[10] = (unsigned char)crc;
+        crc = crc16(&data[9], 2, crc);
+        ASSERT(0 == crc);
+    }
+}
+
+static void test_crc24(void) {
+    {
+        unsigned char data[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        static const unsigned long crc_init = 0;
+        unsigned long crc = crc24(data, COUNT_OF(data), crc_init);
+        ASSERT(0xcde703 == crc);
+
+        crc = crc_init;
+        for (int i = 0; i < COUNT_OF(data); i++) {
+            crc = crc24(&data[i], 1, crc);
+        }
+        ASSERT(0xcde703 == crc);
+    }
+
+    {
+        unsigned char data[] = {
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '0', '0'
+        };
+        static const unsigned long crc_init = 0;
+        unsigned long crc = crc24(data, COUNT_OF(data), crc_init);
+        data[9] = (unsigned char)(crc >> 16u);
+        data[10] = (unsigned char)(crc >> 8u);
+        data[11] = (unsigned char)crc;
+        crc = crc24(&data[9], 3, crc);
+        ASSERT(0 == crc);
+    }
+}
+
+static void test_crc32(void) {
+    {
+        unsigned char data[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        static const unsigned long crc_init = 0xFFFFFFFF;
+        unsigned long crc = crc32(data, COUNT_OF(data), crc_init);
+        ASSERT(0x0376e6e7 == crc);
+
+        crc = crc_init;
+        for (int i = 0; i < COUNT_OF(data); i++) {
+            crc = crc32(&data[i], 1, crc);
+        }
+        ASSERT(0x0376e6e7 == crc);
+    }
+
+    {
+        unsigned char data[] = {
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '0', '0', '0'
+        };
+        static const unsigned long crc_init = 0;
+        unsigned long crc = crc32(data, COUNT_OF(data), crc_init);
+        data[9] = (unsigned char)(crc >> 24U);
+        data[10] = (unsigned char)(crc >> 16U);
+        data[11] = (unsigned char)(crc >> 8U);
+        data[12] = (unsigned char)crc;
+        crc = crc32(&data[9], 4, crc);
+        ASSERT(0 == crc);
+    }
+}
+
+int main(void) {
+    test_crc16();
+    test_crc24();
+    test_crc32();
+
+    return 0;
+}
