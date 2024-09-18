ed86ea00ff479fa2b7b47bb0b56108cf9e4cc484
Adel Mamin <adel.mamin@exafore.com>
2024-09-18 19:36:10 +0300

    Add BT force_failure node

--- lib/bt/bt.h
+++ lib/bt/bt.h
@@ -141,9 +141,23 @@ extern const struct am_event am_bt_evt_failure;
  * Complies to am_hsm_state_fn type.
  */
 enum am_hsm_rc am_bt_invert(struct am_hsm *hsm, const struct am_event *event);
+
+/**
+ * Force AM_BT_EVT_SUCCESS of substate operation and send it to superstate.
+ * The substate is expected to return AM_BT_EVT_SUCCESS or AM_BT_EVT_FAILURE
+ * only once. Otherwise behavior is undefined.
+ * Complies to am_hsm_state_fn type.
+ */
 enum am_hsm_rc am_bt_force_success(
     struct am_hsm *hsm, const struct am_event *event
 );
+
+/**
+ * Force AM_BT_EVT_FAILURE of substate operation and send it to superstate.
+ * The substate is expected to return AM_BT_EVT_SUCCESS or AM_BT_EVT_FAILURE
+ * only once. Otherwise behavior is undefined.
+ * Complies to am_hsm_state_fn type.
+ */
 enum am_hsm_rc am_bt_force_failure(
     struct am_hsm *hsm, const struct am_event *event
 );
