f13da54b9812260371baca6beb344350736f66fb
Adel Mamin <adel.mamin@exafore.com>
2024-11-27 14:23:55 +0200

    Fix libs/pal/posix/pal.c compilation errors

--- libs/pal/posix/pal.c
+++ libs/pal/posix/pal.c
@@ -30,48 +30,118 @@
 
 #ifdef AMAST_PAL_POSIX
 
-#include <stddef.h>
+#include <stdbool.h>
+
+#define _GNU_SOURCE
+
 #include <stdint.h>
+#include <stdlib.h>
+
+#define _POSIX_C_SOURCE 200809L
 
 #include <pthread.h>
+#include <time.h>
+#include <bits/local_lim.h>
+#include <sched.h>
+#include <features.h>
 
+#include "common/compiler.h"
+#include "common/macros.h"
 #include "pal/pal.h"
 
+/* to silence unused macro warnings */
+AM_ASSERT_STATIC(_POSIX_C_SOURCE);
+
 #define AM_PAL_TICK_DOMAIN_DEFAULT_MS 10
+
+struct am_pal_task {
+    /* pthread data */
+    pthread_t thread;
+    /* mutex to protect condition variable */
+    pthread_mutex_t mutex;
+    /* condition variable for signaling */
+    pthread_cond_t cond;
+    /* flag to track notification state */
+    bool notified;
+    void *arg;
+    /* entry point */
+    void (*entry)(void *arg);
+};
 
 static pthread_mutex_t critical_section_mutex_ = PTHREAD_MUTEX_INITIALIZER;
 
 void am_pal_crit_enter(void) {
-    int result = pthread_mutex_lock(&critical_section_mutex);
+    int result = pthread_mutex_lock(&critical_section_mutex_);
     AM_ASSERT(0 == result);
 }
 
 void am_pal_crit_exit(void) {
-    int result = pthread_mutex_unlock(&critical_section_mutex);
+    int result = pthread_mutex_unlock(&critical_section_mutex_);
     AM_ASSERT(0 == result);
 }
 
 void *am_pal_task_create(
     const char *name,
-    int priority,
+    const int priority,
     void *stack,
-    int stack_size,
+    const int stack_size,
     void (*entry)(void *arg),
     void *arg
 ) {
-    (void)name;
-    (void)priority;
-    (void)stack;
-    (void)stack_size;
-    (void)entry;
+    AM_ASSERT(stack);
+    AM_ASSERT(stack_size > 0);
+    AM_ASSERT(entry);
+    AM_ASSERT(priority >= 0);
     (void)arg;
 
-    return NULL;
+    pthread_attr_t attr;
+    int ret = pthread_attr_init(&attr);
+    AM_ASSERT(0 == ret);
+
+    ret = pthread_attr_setstacksize(
+        &attr, (size_t)AM_MAX(stack_size, PTHREAD_STACK_MIN)
+    );
+    AM_ASSERT(0 == ret);
+
+    ret = pthread_attr_setschedpolicy(&attr, SCHED_RR);
+    AM_ASSERT(0 == ret);
+
+    int prio = priority + sched_get_priority_min(SCHED_RR);
+    struct sched_param param = {.sched_priority = prio};
+    ret = pthread_attr_setschedparam(&attr, &param);
+    AM_ASSERT(0 == ret);
+    ret = pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+    AM_ASSERT(0 == ret);
+
+    struct am_pal_task *task = malloc(sizeof(struct am_pal_task));
+    AM_ASSERT(0 == task);
+
+    pthread_setname_np(task->thread, name);
+
+    return task;
 }
 
-void am_pal_task_notify(void *task) { (void)task; }
+void am_pal_task_notify(void *task) {
+    AM_ASSERT(task);
 
-void am_pal_task_wait(void *task) { (void)task; }
+    struct am_pal_task *t = (struct am_pal_task *)task;
+    pthread_mutex_lock(&t->mutex);
+    t->notified = true;
+    pthread_cond_signal(&t->cond);
+    pthread_mutex_unlock(&t->mutex);
+}
+
+void am_pal_task_wait(void *task) {
+    AM_ASSERT(task);
+
+    struct am_pal_task *t = (struct am_pal_task *)task;
+    pthread_mutex_lock(&t->mutex);
+    while (!t->notified) {
+        pthread_cond_wait(&t->cond, &t->mutex);
+    }
+    t->notified = false;
+    pthread_mutex_unlock(&t->mutex);
+}
 
 uint32_t am_pal_time_get_ms(void) {
     struct timespec ts;
@@ -84,12 +154,12 @@ uint32_t am_pal_time_get_tick(int domain) {
     AM_ASSERT(AM_PAL_TICK_DOMAIN_DEFAULT == domain);
 
     uint32_t ms = am_pal_time_get_ms();
-    return (uint32_t)AM_DIVIDE_ROUND_UP(ms / AM_PAL_TICK_DOMAIN_DEFAULT_MS);
+    return (uint32_t)AM_DIVIDE_ROUND_UP(ms, AM_PAL_TICK_DOMAIN_DEFAULT_MS);
 }
 
 uint32_t am_pal_time_get_tick_from_ms(int domain, uint32_t ms) {
     AM_ASSERT(AM_PAL_TICK_DOMAIN_DEFAULT == domain);
-    return (uint32_t)AM_DIVIDE_ROUND_UP(ms / AM_PAL_TICK_DOMAIN_DEFAULT_MS);
+    return (uint32_t)AM_DIVIDE_ROUND_UP(ms, AM_PAL_TICK_DOMAIN_DEFAULT_MS);
 }
 
 uint32_t am_pal_time_get_ms_from_tick(int domain, uint32_t tick) {
@@ -111,12 +181,11 @@ void am_pal_sleep_ms(int ms) {
         return;
     }
     AM_ASSERT(ms > 0);
-    struct timespec ts = {
-        /* convert milliseconds to seconds */
-        .tv_sec = ms / 1000,
-        /* remaining milliseconds to nanoseconds */
-        .tv_nsec = (ms % 1000) * 1000000
-    }
+    struct timespec ts = {/* convert milliseconds to seconds */
+                          .tv_sec = ms / 1000,
+                          /* remaining milliseconds to nanoseconds */
+                          .tv_nsec = (ms % 1000) * 1000000
+    };
     nanosleep(&ts, /*rmtp=*/NULL);
 }
 
