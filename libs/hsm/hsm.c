6e78351da4841e1938a7e67ec0a0d332d3a4f8ae
Adel Mamin <adel.mamin@exafore.com>
2025-01-31 21:26:31 +0200

    Fix HSM comments doxygen formatting

--- libs/hsm/hsm.c
+++ libs/hsm/hsm.c
@@ -61,8 +61,8 @@ static void hsm_set_state(struct am_hsm *hsm, struct am_hsm_state s) {
 /**
  * Build ancestor chain path.
  *
- * The path starts with #from state and ends with substate of #until state OR
- * with state #till, if it is provided.
+ * The path starts with \p from state and ends with substate of \p until state
+ * OR with state \p till, if it is provided.
  *
  * @param hsm    HSM handler
  * @param path   the path is placed here
@@ -136,7 +136,7 @@ static void hsm_exit_state(struct am_hsm *hsm) {
 /**
  * Exit states.
  *
- * Start with current and end with immediate substate of #until.
+ * Start with current and end with immediate substate of \p until.
  *
  * @param hsm    exit the states of this HSM
  * @param until  stop the exit when reaching this state without exiting it
@@ -150,10 +150,10 @@ static void hsm_exit(struct am_hsm *hsm, struct am_hsm_state until) {
 /**
  * Recursively enter and init destination state.
  *
- * 1. enter all states in #path
- * 2. init destination state stored in #path[0]
+ * 1. enter all states in \p path
+ * 2. init destination state stored in \p path[0]
  * 3. if destination state requested an initial transition, then build
- *    new #path and go to step 1
+ *    new \p path and go to step 1
  *
  * @param hsm   enter and init the states of this HSM
  * @param path  the path to enter
